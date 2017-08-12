54f912c6a02363a017516c3ee9a03b2fcc013e44
ZephrKoo <30658485+Zephyr-Koo@users.noreply.github.com>
2017-08-12 18:45:16 +0800

    Update comment of MathQuizExtra.c

--- MathQuizExtra.c
+++ MathQuizExtra.c
@@ -1,6 +1,6 @@
 /*
-Name        : Koo Tian Yaw
-Description : Programming Concepts and Design I (Assignment Demo)
+    Name        : Koo Tian Yaw
+    Description : Programming Concepts and Design I (Assignment Demo)
 */
 
 #include <stdio.h>
@@ -8,7 +8,7 @@ Description : Programming Concepts and Design I (Assignment Demo)
 #include <ctype.h>
 #include <time.h>
 
-#pragma warning(disable: 4996) // required to suppress deprecated function call (e.g. scanf)
+#pragma warning(disable: 4996) // required to suppress warning of deprecated function call (e.g. scanf)
 
 #define OPERATION_ADDITION       1
 #define OPERATION_SUBTRACTION    2
